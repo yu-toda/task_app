f6ed6112eb66d3cfa7174a4582eb9664d6d520db
toda <toda@email.com>
2023-02-06 04:56:48 +0000

    Change from HTTPS to SSH 1

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,6 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
-     
       redirect_to user
     else
       flash.now[:danger] = '認証に失敗しました。'
